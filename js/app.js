188620aba8d29d706b056160d2f39a41f7be2c0f
Austin Chiatto <chiattoaustin@gmail.com>
2023-06-10 11:57:06 -0700

    add player projectile & enemy collision

--- js/app.js
+++ js/app.js
@@ -87,7 +87,7 @@ class Player {
         // create a new image object
         const image = new Image();
         // set image path to path of image
-        image.src = './assets/player/player-ship.png';
+        image.src = './assets/player/player-ship.gif';
         // when the image loads, set image and dimensions
         image.onload = () => {
             this.image = image;
@@ -96,7 +96,7 @@ class Player {
             // define player position
             this.position = {
                 x: (canvas.width / 2) - (this.width / 2),
-                y: (canvas.height / 2) - (this.height / 2)
+                y: (canvas.height) - (this.height * 2)
             };
         }
     };
@@ -124,7 +124,52 @@ class Player {
 
 // create a new player object
 const player = new Player();
-// set all props as false
+
+// Player Projectiles
+// ===========================
+const projectileSpeed = -10;
+class PlayerProjectile {
+    constructor({position, velocity}){
+        this.position = position;
+        this.velocity = velocity;
+
+         // create a new image object
+         const projectile = new Image();
+         // set image path to path of image
+         projectile.src = './assets/player/bullet-001.png';
+         // when the image loads, set image and dimensions
+         projectile.onload = () => {
+            this.image = projectile;
+            this.width = projectile.width * 2;
+            this.height = projectile.height * 2;
+         }
+    };
+
+    // draw image method
+    draw() {
+        // if the image exists, set props
+        c.drawImage(
+            this.image,
+            this.position.x,
+            this.position.y,
+            this.width,
+            this.height
+        );
+    };
+
+    update() {
+        if (this.image) {
+        this.draw();
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+        }
+    };
+};
+
+const playerProjectiles = [];
+
+// Player Control Object
+// ===========================
 const controls = {
     left: {
         pressed: false
@@ -146,6 +191,64 @@ const controls = {
     }
 }
 
+// enemy
+// ===========================
+class Enemy {
+    constructor({position, velocity}) {
+        this.position = position;
+        this.velocity = velocity;
+
+        // create a new image object
+        const image = new Image();
+        // set image path to path of image
+        image.src = './assets/enemy/enemy-placeholder.png';
+        // when the image loads, set image and dimensions
+        image.onload = () => {
+            this.image = image;
+            this.width = image.width;
+            this.height = image.height;
+            // define enemy position
+            this.position = {
+                x: (canvas.width / 2) - (this.width / 2),
+                y: (canvas.height / 2) - (this.height / 2)
+            };
+        }
+    };
+
+    // draw image method
+    draw() {
+        // if the image exists, set props
+        c.drawImage(
+            this.image,
+            this.position.x,
+            this.position.y,
+            this.width,
+            this.height
+        );
+    };
+
+    update() {
+        if (this.image) {
+            this.draw();
+            this.position.x += this.velocity.x;
+            this.position.y += this.velocity.y;
+        }
+    };
+};
+
+const enemies = [];
+
+enemies.push(new Enemy({
+    position: {
+        x: (canvas.width / 2) - (this.width / 2),
+        y: (canvas.height / 2) - (this.height / 2)
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    }
+}));
+
 // animation loop
 // ===========================
 // continuously draws items
@@ -155,6 +258,55 @@ function render() {
     // objects
     backdrop.update();
     player.update();
+
+    // animate projectiles
+    playerProjectiles.forEach((projectile, i) => {
+        // if the projectile exits the top of the canvas
+        if (projectile.position.y + projectile.height <= 0){
+            // delete the object from the array
+            setTimeout(() => {
+                playerProjectiles.splice(i, 1);
+            }, 0);
+        } else {
+            projectile.update();
+        }
+    });
+
+    // enemy
+    enemies.forEach((enemy, i) => {
+        enemy.update();
+
+        // enemy player projectile collision
+        playerProjectiles.forEach((projectile, j) => {
+            if (
+                (projectile.position.y - (projectile.height) <= enemy.position.y + enemy.height)
+                &&
+                (projectile.position.x + projectile.width >= enemy.position.x)
+                &&
+                (projectile.position.x - projectile.width <= enemy.position.x + enemy.width)
+                &&
+                (projectile.position.y + projectile.height >= enemy.position.y )
+                ) {
+                setTimeout(() => {
+                    // finds the enemy in the enemies array
+                    const enemyExists = enemies.find((foundEnemy) => {
+                        return foundEnemy === enemy;
+                    })
+                    // finds the projectile in the playerProjectiles array
+                    const projectileExists = playerProjectiles.find((foundProjectile) => {
+                        return foundProjectile === projectile;
+                    })
+
+                    // if the enemy that is being removed exists inside the enemies array
+                    // splice/remove it from the array
+                    if (enemyExists && projectileExists) {
+                        enemies.splice(i, 1);
+                        playerProjectiles.splice(j, 1);
+                    }
+                }, 0);
+            }
+        });
+    });
 
     // controls
     // ===========================
@@ -186,28 +338,33 @@ addEventListener('keydown', ({ key }) => {
     // switch statement to check if/what key has been pressed down
     switch(key) {
         case 'ArrowLeft':
-            console.log("left");
             controls.left.pressed = true;
             break
         case 'ArrowRight':
-            console.log("right")
             controls.right.pressed = true;
             break
         case 'ArrowUp':
-            console.log("up")
             controls.up.pressed = true;
             break
         case 'ArrowDown':
-            console.log("down")
             controls.down.pressed = true;
             break
         case 'x':
-            console.log("shield")
+            console.log("shield");
             controls.x.pressed = true;
             break
         case 'z':
-            console.log("shoot")
             controls.z.pressed = true;
+            playerProjectiles.push(new PlayerProjectile({
+                position: {
+                    x: player.position.x +(player.width / 2),
+                    y: player.position.y
+                },
+                velocity: {
+                    x: 0,
+                    y: projectileSpeed
+                }
+            }));
             break
     }
 });
@@ -216,28 +373,23 @@ addEventListener('keyup', ({ key }) => {
     // switch statement to check if/what key has been un-pressed
     switch(key) {
         case 'ArrowLeft':
-            console.log("left");
             controls.left.pressed = false;
             break
         case 'ArrowRight':
-            console.log("right")
             controls.right.pressed = false;
             break
         case 'ArrowUp':
-            console.log("up")
             controls.up.pressed = false;
             break
         case 'ArrowDown':
-            console.log("down")
             controls.down.pressed = false;
             break
         case 'x':
-            console.log("shield")
             controls.x.pressed = false;
             break
         case 'z':
-            console.log("shoot")
             controls.z.pressed = false;
             break
     }
 });
+
