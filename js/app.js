40a9428cb20ec2b403ca4aa350d220baad56d05b
Austin Chiatto <chiattoaustin@gmail.com>
2023-06-09 18:59:36 -0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// Global Variables
+// ===========================
+const canvas = document.querySelector(".game-canvas");
+const c = canvas.getContext('2d');
+
+// Canvas Dimensions
+// ===========================
+canvas.width = 800
+canvas.height = innerHeight - 64;
